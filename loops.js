7858bee07fc5f542a0c29b563c27a3d894cc3d8f
Kaitlyn Robey <katie.elena2000@gmail.com>
2018-01-09 01:11:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,9 @@
 function forLoop(array) {
   for(var i === 0, i<26, i++) {
     if(i===1) {
-      array.push ('I am 1 strange loop')
-    }
-    else {
-      array.push ('I am ${i} strange loops')
+      array.push('I am 1 strange loop')
+    } else {
+      array.push('I am ${i} strange loops')
     }
   }
 
